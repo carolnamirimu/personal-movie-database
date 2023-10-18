31ed6f619911d2fa3f0a03ee472aa91b1593c7bc
carolnamirimu <namirimu1000@gmail.com>
2023-10-18 11:58:45 +0300

    created the navigation bar

---
+++ app/NavBar.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import { MdLocalMovies } from 'react-icons/md';
+
+const NavBar = () => {
+  return (
+      <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center '>
+          <span><MdLocalMovies classname='logo'/></span>
+          <ul className='flex space-x-6'>
+              <li>
+                  <a href='/'>Select Movies</a>
+</li>
+             <li><a href='/'>Personal Movie Database</a></li>
+              <li><a href='/'>About me</a></li>
+          </ul>
+
+   </nav>
+  )
+}
+
+export default NavBar
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-
-
+import Image from "next/image"
 export default function Home() {
   return (
     <main>
       <h1>
         Personal Movie Database
+        
       </h1>
    </main>
   )
