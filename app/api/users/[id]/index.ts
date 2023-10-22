2115f00018febf37538b53c3453d18becda90ddd
carolnamirimu <namirimu1000@gmail.com>
2023-10-23 02:57:44 +0300

    created an api file

---
+++ app/api/users/[id]/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const prisma = new PrismaClient();
+  if (req.method === "GET") {
+    const users = await prisma.user.findMany();
+    return res.send(users);
+  } else if (req.method === "post") {
+    res.status(201).send("POST");
+  }
+}
